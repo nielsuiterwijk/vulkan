2e62c06f332adf27a2e9d288aeee0675c83c47e2
Niels.Uiterwijk <nielsuiterwijk@gmail.com>
2018-10-02 23:26:11 +0200

    added json descriptors for shaders

--- shaders/compile.bat
+++ shaders/compile.bat
@@ -1,4 +1,5 @@
 IF NOT EXIST "%cd%/../assets/shaders/" mkdir "%cd%/../assets/shaders/"
 
 @for %%I IN (*.vert; *.tesc; *.tese; *.geom; *.frag; *.comp) DO (%VULKAN_SDK%/Bin32/glslangValidator.exe -V "%%I" -o "../assets/shaders/%%~nI%%~xI.spv")
+@for %%I IN (*.vert; *.tesc; *.tese; *.geom; *.frag; *.comp) DO (SPIRV-Cross.exe "../assets/shaders/%%~nI%%~xI.spv" --output "../assets/shaders/%%~nI%%~xI.json" --reflect)
 pause
