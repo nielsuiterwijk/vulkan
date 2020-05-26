03249b03f8d83674d67d775215d97ad054692d23
Gorbear <nielsuiterwijk@gmail.com>
2020-05-26 23:32:20 +0200

    Updated shader compilation tools

--- shaders/compile.bat
+++ shaders/compile.bat
@@ -1,5 +1,8 @@
+IF EXIST "%cd%/intermediate/" RMDIR /S /Q "%cd%/intermediate/"
 IF NOT EXIST "%cd%/../assets/shaders/" mkdir "%cd%/../assets/shaders/"
+IF NOT EXIST "%cd%/intermediate/" mkdir "%cd%/intermediate/"
 
-@for %%I IN (*.vert; *.tesc; *.tese; *.geom; *.frag; *.comp) DO (%VULKAN_SDK%/Bin32/glslangValidator.exe -V "%%I" -o "../assets/shaders/%%~nI%%~xI.spv")
-@for %%I IN (*.vert; *.tesc; *.tese; *.geom; *.frag; *.comp) DO (SPIRV-Cross.exe "../assets/shaders/%%~nI%%~xI.spv" --output "../assets/shaders/%%~nI%%~xI.json" --reflect)
+@for %%I IN (*.vert; *.tesc; *.tese; *.geom; *.frag; *.comp) DO (tools\\glslc.exe -g "%%I" -o "./intermediate/%%~nI%%~xI.spv")
+@for %%I IN (*.vert; *.tesc; *.tese; *.geom; *.frag; *.comp) DO (tools\\spirv-cross.exe "./intermediate/%%~nI%%~xI.spv" --output "../assets/shaders/%%~nI%%~xI.json" --reflect)
+@for %%I IN (*.vert; *.tesc; *.tese; *.geom; *.frag; *.comp) DO (tools\\spirv-opt.exe -O "./intermediate/%%~nI%%~xI.spv" -o "../assets/shaders/%%~nI%%~xI.spv")
 pause
